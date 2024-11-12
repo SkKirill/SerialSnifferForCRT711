fae8f94cd632e5797361313c894f8bd12896f1e0
Kirill <128813627+SkKirill@users.noreply.github.com>
2024-11-12 15:00:38 +0300

    New dataToSend

--- Program.cs
+++ Program.cs
@@ -1,52 +1,73 @@
 ﻿using System.IO.Ports;
 
-var baudRates = new[] { 9600, 19200, 38400, 57600 };
-const string serialPortName = "/dev/ttyUSB0";
-
-try
+internal class Program
 {
-    const int valueCMT = 0x43; // (byte)'C'
+    private const int ValueSTX = 0xF2; // Start Character
+    private const int ValueADDR = 0x00; // 
+    private const int ValueLENH = 0x00; // 
+    private const int ValueLENL = 0x00; // 
+    private const int ValueCmt = 0x43; // (byte)'C'
+    private const int ValueCm = 0x30; // 30H
+    private const int ValuePm = 0x34; // 34H
+    private const int ValueData = 0x00; //
+    private const int ValueETX = 0x00; //
+    private const int ValueBCC = 0x00; //
     
-    foreach (var baudRate in baudRates)
+    
+    
+    private static readonly int[] BaudRates = { 9600, 19200, 38400, 57600 };
+    private const string SerialPortName = "/dev/ttyUSB0";
+
+    public static async Task Main(string[] args)
     {
-        SerialPort port;
-        // Открываем соединение с портом в новом baudRate
+        await MethodIoPorts();
+    }
+
+    private static async Task MethodIoPorts()
+    {
         try
         {
-            port = new SerialPort(serialPortName, baudRate: baudRate);
-            port.Open();
+            foreach (var baudRate in BaudRates)
+            {
+                SerialPort port;
+                // Открываем соединение с портом в новом baudRate
+                try
+                {
+                    port = new SerialPort(SerialPortName, baudRate: baudRate);
+                    port.Open();
+                }
+                catch (Exception exception)
+                {
+                    Console.WriteLine($"Не удалось подключить порт {SerialPortName}" + exception.Message);
+                    continue;
+                }
+
+                byte[] dataToSend = new byte[] { 0xF2, 0x00, 0x00, 0x07, 0x43, 0x30, 0x34, 0x03, 0x75 };
+                port.Write(dataToSend, 0, dataToSend.Length);
+
+                Console.Write($"Отправили port={SerialPortName} baudRate={baudRate} -> ");
+                foreach (var item in dataToSend)
+                {
+                    Console.Write($"0x{item:X2}  ");
+                }
+
+                Console.WriteLine();
+
+                await Task.Delay(1000);
+                Console.Write("Получили: ");
+                while (port.BytesToRead > 0)
+                {
+                    Console.Write($"0x{port.ReadByte():X2}, ");
+                }
+
+
+                Console.WriteLine();
+                port.Close();
+            }
         }
         catch (Exception exception)
         {
-            Console.WriteLine($"Не удалось подключить порт {serialPortName}" + exception.Message);
-            continue;
+            Console.WriteLine(exception);
         }
-
-        var dataToSend = new byte[] { valueCMT };
-
-        port.Write(dataToSend, 0, dataToSend.Length);
-
-        Console.Write($"Отправили port={serialPortName} baudRate={baudRate} -> ");
-        foreach (var item in dataToSend)
-        {
-            Console.Write($"0x{item:X2}  ");
-        }
-
-        Console.WriteLine();
-
-        await Task.Delay(1000);
-        Console.Write("Получили: ");
-        while (port.BytesToRead > 0)
-        {
-            Console.Write($"0x{port.ReadByte():X2}, ");
-        }
-
-
-        Console.WriteLine();
-        port.Close();
     }
-}
-catch (Exception exception)
-{
-    Console.WriteLine(exception);
 }
