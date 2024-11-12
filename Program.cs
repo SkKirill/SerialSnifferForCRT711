56e31d7bd20c209691056f64b9435ea4f67bb17e
Kirill <128813627+SkKirill@users.noreply.github.com>
2024-11-12 11:23:47 +0300

    Commit

--- Program.cs
+++ Program.cs
@@ -1,40 +1,50 @@
 ﻿using System.IO.Ports;
 
-var baudRate = 115200;
-var description = "считыватель карт";
-var SerialPortName = "COM6";
-
-Dictionary<string, SerialPort> serialPorts = new();
+var baudRates = new[] { 9600, 19200, 38400, 57600 };
+const string serialPortName = "/dev/ttyUSB0";
 
 try
 {
-    try
-    {
-        var newPort = new SerialPort(SerialPortName, baudRate);
-        newPort.Open();
-        serialPorts.Add(SerialPortName, newPort);
-    }
-    catch (Exception exception)
-    {
-        Console.WriteLine($"Не удалось подключить порт {SerialPortName}" + exception.Message);
-    }
-
-    const int value30H = 0x30; //0b0001_1110;
-    const int valuePm = 031; //0b0001_1111;
-    var dataToSend = new byte[] { (byte)'C', value30H, valuePm};
+    const int valueCMT = 0x43; // (byte)'C'
     
-    var port = serialPorts[SerialPortName];
-    
-    port.Write(dataToSend, 0, dataToSend.Length);
-    Console.WriteLine($"Отправили на порт {SerialPortName}: {value30H}");
-
-    Console.Write("Получили: ");
-    while (port.BytesToRead > 0)
+    foreach (var baudRate in baudRates)
     {
-        Console.Write($"0x{port.ReadByte():X2}, ");
-    }
+        SerialPort port;
+        // Открываем соединение с портом в новом baudRate
+        try
+        {
+            port = new SerialPort(serialPortName, baudRate: baudRate);
+            port.Open();
+        }
+        catch (Exception exception)
+        {
+            Console.WriteLine($"Не удалось подключить порт {serialPortName}" + exception.Message);
+            continue;
+        }
 
-    Console.WriteLine();
+        var dataToSend = new byte[] { valueCMT };
+
+        port.Write(dataToSend, 0, dataToSend.Length);
+
+        Console.Write($"Отправили port={serialPortName} baudRate={baudRate} -> ");
+        foreach (var item in dataToSend)
+        {
+            Console.Write($"0x{item:X2}  ");
+        }
+
+        Console.WriteLine();
+
+        await Task.Delay(1000);
+        Console.Write("Получили: ");
+        while (port.BytesToRead > 0)
+        {
+            Console.Write($"0x{port.ReadByte():X2}, ");
+        }
+
+
+        Console.WriteLine();
+        port.Close();
+    }
 }
 catch (Exception exception)
 {
